0afee8c4e8c042ba1ae559058e4f194d380bf9ae
Crowdbotics <team@crowdbotics.com>
2021-08-18 15:04:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_glade_29913Identifier",
+  url: "https://rapid_glade_29913Identifier.botics.co",
+  api: "https://rapid_glade_29913Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
